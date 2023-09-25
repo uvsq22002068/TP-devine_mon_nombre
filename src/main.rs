524ea0843368104a3b771512a90f8df388a116f9
uvsq22002068 <71604192+uvsq22002068@users.noreply.github.com>
2023-09-25 17:15:40 +0200

    feat: loop the code

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,34 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-fn main() -> io::Result<()> {
-    println!(
-        "Devine mon nombre ! 
-    
-Saisissez votre proposition :"
-    );
-
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
+fn main() -> anyhogiw::Result<()> {
     let rng = rand::thread_rng().gen_range(1..101);
 
-    println!("valeur entree : {}", buffer.trim());
-    println!("valeur aléatoire : {rng}");
+    loop {
+        println!(
+            "Devine mon nombre ! 
+    
+Saisissez votre proposition :"
+        );
 
-    let n = buffer.trim().parse::<u32>().unwrap();
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
 
-    let message = match n.cmp(&rng) {
-        Ordering::Equal => "le nombre entré est égal au nombre aléatoire",
-        Ordering::Greater => "le nombre entré est plus grand que le nombre aléatoire",
-        Ordering::Less => "le nombre entré est plus petit que le nombre aléatoire",
-    };
+        println!("valeur entree : {}", buffer.trim());
+        println!("valeur aléatoire : {rng}");
 
-    println!("{message}");
+        let n = buffer.trim().parse::<u32>()?;
+        let message = match n.cmp(&rng) {
+            Ordering::Equal => "le nombre entré est égal au nombre aléatoire",
+            Ordering::Greater => "le nombre entré est plus grand que le nombre aléatoire",
+            Ordering::Less => "le nombre entré est plus petit que le nombre aléatoire",
+        };
+
+        println!("{message}");
+        if n == rng {
+            break;
+        }
+    }
 
     Ok(())
 }
