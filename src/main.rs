6c7c55e097e62d5336501667e939356eeda9073f
uvsq22002068 <71604192+uvsq22002068@users.noreply.github.com>
2023-09-25 16:44:00 +0200

    chore: switch to cargo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 fn main() -> io::Result<()>{
     println!("Devine mon nombre ! 
@@ -7,8 +8,10 @@ Saisissez votre proposition :");
 
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer)?;
+    let rng = rand::thread_rng().gen_range(1..101);
 
     println!("valeur entree : {}", buffer.trim());
-
+    println!("valeur aléatoire : {rng}");
+    
     Ok(())
 }
