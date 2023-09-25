236d0acc57c91c85e948a59e57404f6dd0922330
uvsq22002068 <71604192+uvsq22002068@users.noreply.github.com>
2023-09-25 16:32:35 +0200

    feat: read user input and print them

--- src/devine_mon_nombre.rs
+++ src/devine_mon_nombre.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello World!");
+use std::io;
+
+fn main() -> io::Result<()>{
+    println!("Devine mon nombre ! 
+    
+Saisissez votre proposition :");
+
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+
+    println!("valeur entree : {}", buffer.trim());
+
+    Ok(())
 }
